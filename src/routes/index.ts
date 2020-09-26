246b34f589d35b2ad9279180b57719a2142de64a
Periklis <pericleskb@gmail.com>
2020-09-26 12:04:12 +0300

    using mongoose and storing data version variable

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,19 +2,39 @@ import * as express from "express"
 import * as bodyParser from "body-parser"
 import mongoClient from "mongodb"
 import { env } from "process"
-import { isNumber } from "util"
+import Recipe from "../models/recipe"
+import DbVariables from "../models/dbVariable"
+import mongoose from "mongoose"
 
 export const register =   (app: express.Application) => {
-  const dbUrl = process.env.DATABASE_URL
   const envVar = process.env;
-
+  let dataVersion = 0;
   let db : mongoClient.Db;
+
+  mongoose.connect(envVar.DATABASE_URL, {useNewUrlParser: true})
+
   mongoClient.connect(envVar.DATABASE_URL)
     .then (async client => {
       console.log(`Connection to database established`)
       db = client.db(envVar.DB_NAME)
 
-      let cursor =  db.listCollections({name: 'recipe_details_view'})
+      let cursor = db.listCollections({name: envVar.VARIABLES_COLLECTION})
+      if (!await cursor.hasNext()) {
+        const dataVersionVar = new DbVariables();
+        dataVersionVar.name = "data_version"
+        dataVersionVar.value = dataVersion;
+        dataVersionVar.save();
+      } else {
+         DbVariables.findOne({name: "data_version"})
+         .then (doc => {
+           dataVersion = doc.value.valueOf()
+         })
+         .catch (err => {
+           console.log("Did not find data version variable. Set to 0")
+         })
+      }
+
+      cursor =  db.listCollections({name: 'recipe_details_view'})
       if (!await cursor.hasNext()) {
         db.createCollection('recipe_details_view', {
                   viewOn: env.RECIPES_COLLECTION,
@@ -39,49 +59,69 @@ export const register =   (app: express.Application) => {
   app.use(bodyParser.json())
 
   app.post('/recipe/add/', async (req, res) => {
-    try {
-      db.collection(envVar.RECIPES_COLLECTION).insertOne(req.body)
+    const recipe = new Recipe(req.body)
+    recipe.dataVersion = dataVersion
+    recipe.save()
+    .then (async savedRecipe => {
       res.status(200)
-    } catch (err) {
+      await DbVariables.updateOne({name: "data_version"}, {value: ++dataVersion})
+    })
+    .catch (err => {
+      if (err.code === 11000) {
+        console.error("Tried to add recipe with existing title")
+        res.json({error: "Recipe name already exists"})
+      } else {
       console.error(err)
       res.json( {error: err.message || err })
-    }
+      }
+    })
   })
 
   app.get(`/recipeDetails/:recipeName/`, async (req, res) => {
-    try {
-      const query = {title: req.params.recipeName}
-      const result = await db.collection('recipe_details_view').findOne(query)
-      res.send(result)
-    } catch (err) {
+    const query = {title: req.params.recipeName}
+    db.collection('recipe_details_view').findOne(query)
+    .then (result => res.send(result))
+    .catch (err => {
       console.error(err)
       res.json( {error: err.message || err })
-    }
+    })
   })
 
   app.get(`/recipeInstructions/:recipeName/`, async (req, res) => {
-    try {
-      const query = {title: req.params.recipeName}
-      const result = await db.collection('recipe_instructions_view').findOne(query)
-      res.send(result)
-    } catch (err) {
+    const query = {title: req.params.recipeName}
+    db.collection('recipe_instructions_view').findOne(query)
+    .then (result => res.send(result))
+    .catch (err => {
       console.error(err)
       res.json( {error: err.message || err })
-    }
+    })
   })
 
-  app.get(`/recipeDetails/from/:from/to/:to/`, async (req, res) => {
-    if (!isNaN(+req.params.from) && !isNaN(+req.params.to)) {
-      db.collection('recipe_details_view').find().toArray()
-      .then( array => {
-        res.send(array.slice(+req.params.from, +req.params.to))
-      })
-      .catch(err => {
-        console.log(err)
-        res.status(500).send()
-      })
-    } else {
-      res.status(400).send("Route parameters must be numbers")
-    }
+  app.get(`/recipeDetails/`, async (req, res) => {
+    db.collection('recipe_details_view').find().toArray()
+    .then( array => {
+      res.setHeader("data_version", dataVersion)
+      res.send(array)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).send()
+    })
   })
+
+  // Getting recipe details in increments
+  // app.get(`/recipeDetails/from/:from/to/:to/`, async (req, res) => {
+  //   if (!isNaN(+req.params.from) && !isNaN(+req.params.to)) {
+  //     db.collection('recipe_details_view').find().toArray()
+  //     .then( array => {
+  //       res.send(array.slice(+req.params.from, +req.params.to))
+  //     })
+  //     .catch(err => {
+  //       console.log(err)
+  //       res.status(500).send()
+  //     })
+  //   } else {
+  //     res.status(400).send("Route parameters must be numbers")
+  //   }
+  // })
 }
